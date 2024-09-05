4b9e900046d711c5ad491b772a5cd6ec227e036e
Sam A <98753696+samfisherirl@users.noreply.github.com>
2024-09-05 01:31:36 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,7 @@
-D:\RVC1006Nvidia\venv\Scripts\pyinstaller --windowed --icon=app.ico --noconfirm --add-data "D:\RVC1006Nvidia\venv\Lib\site-packages\webview*;webview" --clean webviewer.py
-D:\RVC1006Nvidia\venv\Scripts\pyinstaller --windowed --icon=app.ico --noconfirm --add-data "D:\RVC1006Nvidia\venv\Lib\site-packages\sv_ttk*;sv_ttk"  --clean RyleTTS.py
-D:\RVC1006Nvidia\venv\Scripts\pyinstaller --console --icon=app.ico --noconfirm --clean sock.py
+CALL venv\Scripts\activate 
+
+venv\Scripts\pip.exe install pyinstaller setuptools
+
+.\venv\Scripts\pyinstaller --windowed --icon=app.ico --noconfirm --add-data "D:\RVC1006Nvidia\venv\Lib\site-packages\webview*;webview" --clean webviewer.py
+.\venv\Scripts\pyinstaller --windowed --icon=app.ico --noconfirm --add-data "D:\RVC1006Nvidia\venv\Lib\site-packages\sv_ttk*;sv_ttk"  --clean RyleTTS.py
+.\venv\Scripts\pyinstaller --console --icon=app.ico --noconfirm --clean sock.py
