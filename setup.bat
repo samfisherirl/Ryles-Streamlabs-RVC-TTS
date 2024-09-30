8fd5952718c44f0a72f248aed93545fb813fc155
Sam A <98753696+samfisherirl@users.noreply.github.com>
2024-09-30 17:49:06 -0400

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -1,7 +1,5 @@
-CALL venv\Scripts\activate 
+CALL .\venv\Scripts\python.exe -m pip install --upgrade setuptools pyinstaller sv-ttk
+CALL .\venv\Scripts\pyinstaller --windowed --icon=app.ico --noconfirm --add-data "venv\lib\site-packages\sv_ttk;sv_ttk"  --clean RyleTTS.py
 
-venv\Scripts\pip.exe install pyinstaller setuptools
-
-.\venv\Scripts\pyinstaller --windowed --icon=app.ico --noconfirm --add-data "D:\RVC1006Nvidia\venv\Lib\site-packages\webview*;webview" --clean webviewer.py
-.\venv\Scripts\pyinstaller --windowed --icon=app.ico --noconfirm --add-data "D:\RVC1006Nvidia\venv\Lib\site-packages\sv_ttk*;sv_ttk"  --clean RyleTTS.py
-.\venv\Scripts\pyinstaller --console --icon=app.ico --noconfirm --clean sock.py
+CALL .\venv\Scripts\pyinstaller --windowed --icon=app.ico --noconfirm --add-data "venv\Lib\site-packages\webview;webview" --clean webviewer.py
+CALL .\venv\Scripts\pyinstaller --console --icon=app.ico --noconfirm --clean sock.py
